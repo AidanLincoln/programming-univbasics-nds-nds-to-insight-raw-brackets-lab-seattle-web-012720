13ccd278c0153f0fabd504f196f2bf4ffcffbcfa
Aidan Lincoln <aidantlincoln@gmail.com>
2019-12-20 23:43:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,16 +6,15 @@ def directors_totals(nds)
   diretor_index = 0
   movie_index = 0
   
-  #while diretor_index < nds.length do
+  while diretor_index < nds.length do
     total = 0
-    #while movie_index < nds[diretor_index][:movies].length do
-      #total += nds[diretor_index][:movies][movie_index][:worldwide_gross]
-      #hash[nds[diretor_index][:name]] = total
-      #movie_index += 1
-    #end
-  #diretor_index += 1
-  #end
-  #return hash
-  pp nds
+    while movie_index < nds[diretor_index][:movies].length do
+      total += nds[diretor_index][:movies][movie_index][:worldwide_gross]
+      hash[nds[diretor_index][:name]] = total
+      movie_index += 1
+    end
+  diretor_index += 1
+  end
+  return hash
   nil
 end
